b6b3ceac3a98631986bfd7feee56dc48f233d72d
oso1248 <oso1248@gmail.com>
2020-09-05 05:55:49 -0600

    Set to start at index.html
    Need to test csp with axios on front end

--- README.md
+++ README.md
@@ -9,4 +9,8 @@
 [X]Setup static serve folder with template index.html, css, tabulator dist  
 [X]Setup environment variables  
 [X]Setup dbConfig - dev/production  
+[X]Deploy to heroku   
+[X]Add postgres to heroku and configure  
+[X]cl - heroku run knex migrate:latest -a bud-ftc  
+[X]cl - heroku run knex seed:run -a bud-ftc  
 []
--- api/server.js
+++ api/server.js
@@ -9,8 +9,8 @@ server.use(express.json())
 const supplierRouter = require('./routes/rtsSuppliers')
 
 
-// server.use(express.static(path.join(__dirname, '../pages/site')))
-server.get('/', (req, res) => res.json({msg: 'I am always watching'}))
+server.use(express.static(path.join(__dirname, '../pages/site')))
+// server.get('/', (req, res) => res.json({msg: 'I am always watching'}))
 
 
 server.use('/api/supplier', supplierRouter)

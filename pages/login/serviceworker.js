26b1c766ddc8721d299ff40de63fd2ef184d6894
oso1248 <oso1248@gmail.com>
2020-09-19 06:14:10 -0600

    Added install for chrome

---
+++ pages/login/serviceworker.js
@@ -0,0 +1,34 @@
+const staticBudApp = "budApp-v1"
+const assets = [
+  "/",
+  "/index.html",
+  "/commonCSS/styles.css",
+  "",
+  "/contract.png",
+  
+]
+
+
+
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('./serviceworker.js')
+    .then(function() { console.log("Service Worker Registered"); });
+}
+
+
+// self.addEventListener("install", installEvent => {
+//   installEvent.waitUntil(
+//     caches.open(staticBudApp).then(cache => {
+//       cache.addAll(assets)
+//     })
+//   )
+// })
+
+// self.addEventListener("fetch", fetchEvent => {
+//   fetchEvent.respondWith(
+//     caches.match(fetchEvent.request).then(res => {
+//       return res || fetch(fetchEvent.request)
+//     })
+//   )
+// })

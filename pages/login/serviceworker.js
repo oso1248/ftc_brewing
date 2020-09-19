b4f200c68c06c471222d114a68652f6fec000e29
oso1248 <oso1248@gmail.com>
2020-09-19 06:32:08 -0600

    Fixing install again"

--- pages/login/serviceworker.js
+++ pages/login/serviceworker.js
@@ -25,10 +25,10 @@ self.addEventListener("install", installEvent => {
   )
 })
 
-// self.addEventListener("fetch", fetchEvent => {
-//   fetchEvent.respondWith(
-//     caches.match(fetchEvent.request).then(res => {
-//       return res || fetch(fetchEvent.request)
-//     })
-//   )
-// })
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})

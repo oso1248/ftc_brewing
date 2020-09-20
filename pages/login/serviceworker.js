b1d192f16e14d5287d8da7979d1b37df29580745
oso1248 <oso1248@gmail.com>
2020-09-20 04:03:07 -0600

    SW Change

--- pages/login/serviceworker.js
+++ pages/login/serviceworker.js
@@ -28,8 +28,8 @@ self.addEventListener("install", installEvent => {
 self.addEventListener("fetch", fetchEvent => {
   fetchEvent.respondWith(
     caches.match(fetchEvent.request).then(res => {
-      // return res || fetch(fetchEvent.request)
-      fetch(fetchEvent.request) || return res 
+      fetch(fetchEvent.request)
+      // fetch(fetchEvent.request) || return res 
     })
   )
 })

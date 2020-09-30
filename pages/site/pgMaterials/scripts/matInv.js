d521339a50e16f7628b8b549f5da1fd2b9e523ed
oso1248 <oso1248@gmail.com>
2020-09-29 19:56:56 -0600

    Change scanner

--- pages/site/pgMaterials/scripts/matInv.js
+++ pages/site/pgMaterials/scripts/matInv.js
@@ -1,12 +1,12 @@
 function openQRCamera(node) {
-  var reader = new FileReader();
+  var reader = new FileReader()
   reader.onload = function() {
-    node.value = "";
+    node.value = ""
     qrcode.callback = function(res) {
       if(res instanceof Error) {
         alert("No QR code found. Please make sure the QR code is within the camera's frame and try again.")
       } else {
-        //alert(res)
+        alert(res)
         node.parentNode.previousElementSibling.value = res
       }
     }

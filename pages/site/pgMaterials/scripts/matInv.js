dbe8a3d11733ebdc6e9ddcf14814ba4aa2377cfb
oso1248 <oso1248@gmail.com>
2020-09-30 20:19:54 -0600

    Scanner

--- pages/site/pgMaterials/scripts/matInv.js
+++ pages/site/pgMaterials/scripts/matInv.js
@@ -1,5 +1,6 @@
 // function openQRCamera(node) {
-function openQRCamera() {
+async function openQRCamera() {
+  console.log('hello')
   let reader = new FileReader()
   let commodity = document.getElementById('commodity') 
   reader.onload = function() {
@@ -8,14 +9,15 @@ function openQRCamera() {
       if(res instanceof Error) {
         alert("No QR code found. Please make sure the QR code is within the camera's frame and try again.")
       } else {
-        // alert(res) 
+        alert(res) 
         // node.parentNode.previousElementSibling.value = res
         commodity.value = res
       }
     }
     qrcode.decode(reader.result)
   }
+  console.log('goodbye')
   // reader.readAsDataURL(node.files[0])
 }
 
-// document.getElementById('scanner').addEventListener('click', openQRCamera)
+document.getElementById('scanner').addEventListener('click', openQRCamera)

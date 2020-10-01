45cd36102615b7d3078149008863d6fce397a2fa
oso1248 <oso1248@gmail.com>
2020-10-01 17:39:22 -0600

    Scanner

--- pages/site/pgMaterials/scripts/matInv.js
+++ pages/site/pgMaterials/scripts/matInv.js
@@ -1,49 +1,16 @@
-// // function openQRCamera(node) {
-// async function openQRCamera() {
-//   console.log('hello')
-//   let reader = new FileReader()
-//   let commodity = document.getElementById('commodity') 
-//   reader.onload = function() {
-//     commodity.value = ""
-//     qrcode.callback = function(res) {
-//       if(res instanceof Error) {
-//         alert("No QR code found. Please make sure the QR code is within the camera's frame and try again.")
-//       } else {
-//         alert(res) 
-//         // node.parentNode.previousElementSibling.value = res
-//         commodity.value = res
-//       }
-//     }
-//     qrcode.decode(reader.result)
-//   }
-//   reader.readAsDataURL(commodity.files[0])
-//   // reader.readAsDataURL(node.files[0])
-// }
-
-
-
 function openQRCamera(node) {
-  var reader = new FileReader();
+  let reader = new FileReader();
   reader.onload = function() {
     node.value = "";
     qrcode.callback = function(res) {
       if(res instanceof Error) {
         alert("No QR code found. Please make sure the QR code is within the camera's frame and try again.")
       } else {
-        alert(res)
-        node.parentNode.previousElementSibling.value = res
+        // alert(res)
+        document.getElementById('comm').value = res
       }
     }
     qrcode.decode(reader.result)
   }
   reader.readAsDataURL(node.files[0])
 }
-
-
-document.getElementById('scanner2').addEventListener('click', scan)
-
-function scan() {
-  document.getElementById('scanner').click()
-}
-
-

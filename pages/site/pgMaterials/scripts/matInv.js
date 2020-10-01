becc4df76c715d8c11b4e1157124d88f146a419c
oso1248 <oso1248@gmail.com>
2020-10-01 16:54:25 -0600

    Scanner

--- pages/site/pgMaterials/scripts/matInv.js
+++ pages/site/pgMaterials/scripts/matInv.js
@@ -1,23 +1,49 @@
-// function openQRCamera(node) {
-async function openQRCamera() {
-  console.log('hello')
-  let reader = new FileReader()
-  let commodity = document.getElementById('commodity') 
+// // function openQRCamera(node) {
+// async function openQRCamera() {
+//   console.log('hello')
+//   let reader = new FileReader()
+//   let commodity = document.getElementById('commodity') 
+//   reader.onload = function() {
+//     commodity.value = ""
+//     qrcode.callback = function(res) {
+//       if(res instanceof Error) {
+//         alert("No QR code found. Please make sure the QR code is within the camera's frame and try again.")
+//       } else {
+//         alert(res) 
+//         // node.parentNode.previousElementSibling.value = res
+//         commodity.value = res
+//       }
+//     }
+//     qrcode.decode(reader.result)
+//   }
+//   reader.readAsDataURL(commodity.files[0])
+//   // reader.readAsDataURL(node.files[0])
+// }
+
+
+
+function openQRCamera(node) {
+  var reader = new FileReader();
   reader.onload = function() {
-    commodity.value = ""
+    node.value = "";
     qrcode.callback = function(res) {
       if(res instanceof Error) {
         alert("No QR code found. Please make sure the QR code is within the camera's frame and try again.")
       } else {
-        alert(res) 
-        // node.parentNode.previousElementSibling.value = res
-        commodity.value = res
+        alert(res)
+        node.parentNode.previousElementSibling.value = res
       }
     }
     qrcode.decode(reader.result)
   }
-  console.log('goodbye')
-  // reader.readAsDataURL(node.files[0])
+  reader.readAsDataURL(node.files[0])
 }
 
-document.getElementById('scanner').addEventListener('click', openQRCamera)
+
+document.getElementById('scanner2').addEventListener('click', scan)
+
+function scan() {
+  document.getElementById('scanner').click()
+}
+
+

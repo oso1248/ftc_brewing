efef331bc5a4cbfefbf811bcf8ef96236459dd39
oso1248 <oso1248@gmail.com>
2020-09-30 19:28:10 -0600

    Scanner stuff

--- pages/site/pgMaterials/scripts/matInv.js
+++ pages/site/pgMaterials/scripts/matInv.js
@@ -1,16 +1,18 @@
 function openQRCamera(node) {
-  var reader = new FileReader()
+  let reader = new FileReader()
   reader.onload = function() {
     node.value = ""
     qrcode.callback = function(res) {
       if(res instanceof Error) {
         alert("No QR code found. Please make sure the QR code is within the camera's frame and try again.")
       } else {
-        alert(res)
+        // alert(res)
         node.parentNode.previousElementSibling.value = res
       }
     }
     qrcode.decode(reader.result)
   }
-  reader.readAsDataURL(node.files[0])
+  // reader.readAsDataURL(node.files[0])
 }
+
+document.getElementById('scanner').addEventListener('click', openQRCamera)

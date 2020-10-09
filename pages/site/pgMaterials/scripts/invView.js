ae46c023f4b3b5e790dad0301b21499c21559e55
oso1248 <oso1248@gmail.com>
2020-10-08 18:25:07 -0600

    inv dates

--- pages/site/pgMaterials/scripts/invView.js
+++ pages/site/pgMaterials/scripts/invView.js
@@ -51,8 +51,8 @@ function invDates() {
       return invDate.map(listItem => {
 
         let invDate = createNode('option')
-        invDate.innerHTML = moment(listItem.date_trunc).format('YYYY-MM-DD')
-        console.log(moment(listItem.date_trunc).format('YYYY-MM-DD'))
+        invDate.innerHTML = moment(listItem.date_trunc).add(1,'day').format('YYYY-MM-DD')
+        console.log(moment(listItem.date_trunc).add(1,'day').format('YYYY-MM-DD'))
         append(invDates, invDate)
       })
     })

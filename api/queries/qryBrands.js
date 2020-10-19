005e26d80a96f2f31dc82426cdba995dd549cfd3
oso1248 <oso1248@gmail.com>
2020-10-18 20:29:34 -0600

    fin brand details

--- api/queries/qryBrands.js
+++ api/queries/qryBrands.js
@@ -197,7 +197,38 @@ function getDetailByNameBrwPost(name) {
     .where({'brw.brand': name})
     .first() 
 }
-
+function getDetailByNameFinPre(name) {
+  return db('brnd_fin AS fin')
+    .join('fltr_pre AS pre','pre.fin_id', '=', 'fin.id')
+    .select(
+      'fin.brand AS Brand',
+      'pre.tk_sch AS Schoene Tank',
+      'pre.lines AS System',
+      'pre.tk_trp AS Trap',
+      'pre.tk_fbt AS Filter Beer Tank',
+      'pre.tk_fill AS Fill Tank',
+      'pre.inj AS Injection',
+      'pre.ctrl AS Control',
+      'pre.note AS Note'
+      )
+    .where({'fin.brand': name})
+    .first() 
+}
+function getDetailByNameFinPost(name) {
+  return db('brnd_fin AS fin')
+    .join('fltr_post AS post','post.fin_id', '=', 'fin.id')
+    .select(
+      'fin.brand AS Brand',
+      'post.tk_sch AS Schoene Tank',
+      'post.lines AS System',
+      'post.tk_trp AS Trap',
+      'post.tk_fbt AS Filter Beer Tank',
+      'post.recover AS Recover',
+      'post.note AS Note'
+      )
+    .where({'fin.brand': name})
+    .first() 
+}
 
 
 module.exports = {
@@ -217,5 +248,7 @@ module.exports = {
   changePck, 
   destroyPck,
   getDetailByNameBrwPre,
-  getDetailByNameBrwPost
+  getDetailByNameBrwPost,
+  getDetailByNameFinPre,
+  getDetailByNameFinPost
 }

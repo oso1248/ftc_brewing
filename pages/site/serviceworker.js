5664f1fa2aabbbf1fe210d1ed7bf86df2aa30871
oso1248 <oso1248@gmail.com>
2020-12-15 17:08:15 -0700

    manifest fix

--- pages/site/serviceworker.js
+++ pages/site/serviceworker.js
@@ -4,7 +4,7 @@ if('serviceWorker' in navigator) {
     .then(function() { console.log('Service Worker Registered') })
 }
 
-var CACHE_STATIC = 'static-v001'
+var CACHE_STATIC = 'static-v002'
 
 
 self.addEventListener('install', function (event) {

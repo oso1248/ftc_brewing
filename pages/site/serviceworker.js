c3e82ba2a33a7fa0bb1c863ce5f0db608b31e9ff
oso1248 <oso1248@gmail.com>
2020-12-22 20:49:48 -0700

    cache version change

--- pages/site/serviceworker.js
+++ pages/site/serviceworker.js
@@ -4,7 +4,7 @@ if('serviceWorker' in navigator) {
     .then(function() { console.log('Service Worker Registered') })
 }
 
-var CACHE_STATIC = 'static-v002'
+var CACHE_STATIC = 'static-v003'
 
 
 self.addEventListener('install', function (event) {

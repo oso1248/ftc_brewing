397727a8eb44ee1deacc8be96931a0b249c9be10
oso1248 <oso1248@gmail.com>
2020-12-29 20:11:04 -0700

    roll cache

--- pages/site/serviceworker.js
+++ pages/site/serviceworker.js
@@ -4,7 +4,7 @@ if('serviceWorker' in navigator) {
     .then(function() { console.log('Service Worker Registered') })
 }
 
-var CACHE_STATIC = 'static-v006'
+var CACHE_STATIC = 'static-v007'
 
 
 self.addEventListener('install', function (event) {

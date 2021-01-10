9617548c1394ac5f4b430ec35f09e328775f92bd
oso1248 <oso1248@gmail.com>
2021-01-10 11:19:48 -0700

    add meta tags to prevent zoom

--- pages/site/serviceworker.js
+++ pages/site/serviceworker.js
@@ -4,7 +4,7 @@ if('serviceWorker' in navigator) {
     .then(function() { console.log('Service Worker Registered') })
 }
 
-var CACHE_STATIC = 'static-v011'
+var CACHE_STATIC = 'static-v012'
 
 
 self.addEventListener('install', function (event) {

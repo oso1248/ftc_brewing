28c722a71b73626082cbb89803d6a0987f96b9cb
oso1248 <oso1248@gmail.com>
2021-01-18 03:01:21 -0700

    working on scanner

--- pages/site/serviceworker.js
+++ pages/site/serviceworker.js
@@ -4,7 +4,7 @@ if('serviceWorker' in navigator) {
     .then(function() { console.log('Service Worker Registered') })
 }
 
-var CACHE_STATIC = 'static-v024'
+var CACHE_STATIC = 'static-v025'
 
 
 self.addEventListener('install', function (event) {

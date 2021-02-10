70ca43febd40503f960ba45f4536358958642867
oso1248 <oso1248@gmail.com>
2021-02-09 19:02:39 -0700

    heroku build failed

--- pages/site/serviceworker.js
+++ pages/site/serviceworker.js
@@ -4,7 +4,7 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-var CACHE_STATIC = 'static-v038';
+var CACHE_STATIC = 'static-v039';
 
 self.addEventListener('install', function (event) {
   self.skipWaiting();

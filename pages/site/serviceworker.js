1659e4051fb38a5878cf46726983fe57b849aa5e
oso1248 <oso1248@gmail.com>
2021-03-16 18:34:22 -0600

    restart app

--- pages/site/serviceworker.js
+++ pages/site/serviceworker.js
@@ -4,7 +4,7 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-var CACHE_STATIC = 'static-v041';
+var CACHE_STATIC = 'static-v043';
 
 self.addEventListener('install', function (event) {
   self.skipWaiting();

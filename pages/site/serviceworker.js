c350893c2e904c0f119f2a1bfe515937429de4b9
oso1248 <oso1248@gmail.com>
2021-05-06 14:16:22 -0600

    fixing is mobile

--- pages/site/serviceworker.js
+++ pages/site/serviceworker.js
@@ -4,7 +4,7 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-var CACHE_STATIC = 'static-v056';
+var CACHE_STATIC = 'static-v057';
 
 self.addEventListener('install', function (event) {
   self.skipWaiting();

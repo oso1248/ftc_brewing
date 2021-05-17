558b770b1554a3436c471b564284d176fd4fc6f8
oso1248 <oso1248@gmail.com>
2021-05-16 18:38:39 -0600

    roll cache

--- pages/site/serviceworker.js
+++ pages/site/serviceworker.js
@@ -4,7 +4,7 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-var CACHE_STATIC = 'static-v060';
+var CACHE_STATIC = 'static-v061';
 
 self.addEventListener('install', function (event) {
   self.skipWaiting();
